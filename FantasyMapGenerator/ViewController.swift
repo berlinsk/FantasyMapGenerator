a8f4124fd07acfed6dcb96559fa31eba3fbbc909
berlinsk <berliniumyar@gmail.com>
2025-08-04 14:14:50 +0300

    Initial Commit

---
+++ FantasyMapGenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FantasyMapGenerator
+//
+//  Created by Берлинский Ярослав Владленович on 04.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
